b05ebb2ba56fa76a5954bb3cb1533f8fade59461
Porama Palittapongarnpim <PoramaTV@gmail.com>
2018-11-25 23:56:39 +0700

    Revert main scoreboard that wit screw up like an potato
    This reverts commit ff2f7db09a6c2c5ee8a7d96f76358be641b13467.

--- web/Basketball1/board.js
+++ web/Basketball1/board.js
@@ -27,6 +27,7 @@ $(document).ready(function () {
     
 
     if (team1Name === 'red') {
+<<<<<<< HEAD
         $("#team-1-logo").attr("src", "../assets/Red-left.png");
         
     }
@@ -58,5 +59,38 @@ $(document).ready(function () {
     if (team2Name === 'blue') {
         $("#team-2-logo").attr("src", "../assets/Blue-right.png");
       
+=======
+        $("#team-1-logo").attr("src", "../assets/vampire_logo.png");
+        $("#team_a_title").html("Vampire");
+    }
+    if (team1Name === 'green') {
+        $("#team-1-logo").attr("src", "../assets/siren_logo.png");
+        $("#team_a_title").html("Siren");
+    }
+    if (team1Name === 'yellow') {
+        $("#team-1-logo").attr("src", "../assets/ninetail_logo.png");
+        $("#team_a_title").html("Ninetail");
+    }
+    if (team1Name === 'blue') {
+        $("#team-1-logo").attr("src", "../assets/werewolf_logo.png");
+        $("#team_a_title").html("Werewolf");
+    }
+
+    if (team2Name === 'red') {
+        $("#team-2-logo").attr("src", "../assets/vampire_logo.png");
+        $("#team_b_title").html("Vampire");
+    }
+    if (team2Name === 'green') {
+        $("#team-2-logo").attr("src", "../assets/siren_logo.png");
+        $("#team_b_title").html("Siren");
+    }
+    if (team2Name === 'yellow') {
+        $("#team-2-logo").attr("src", "../assets/ninetail_logo.png");
+        $("#team_b_title").html("Ninetail");
+    }
+    if (team2Name === 'blue') {
+        $("#team-2-logo").attr("src", "../assets/werewolf_logo.png");
+        $("#team_b_title").html("Werewolf");
+>>>>>>> parent of ff2f7db... latest admin panel
     }
 });
