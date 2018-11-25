1e6f6403f34d88382c92f4bd2e01f60fbabef9c0
wit03 <40893632+wit03@users.noreply.github.com>
2018-11-25 16:32:22 +0700

    Finished Display panel

--- web/Basketball1/board.js
+++ web/Basketball1/board.js
@@ -1,7 +1,31 @@
+var sporttype = "Basketball" //= recieve val from sockt io (from admin/in-game-admin-panel/main.js var sporttype) 
+
 var team1Name = "blue" //= recieve val from sockt io (from admin/in-game-admin-panel/main.js var team1Name) 
 var team2Name = "green"//= recieve val from sockt io (from admin/in-game-admin-panel/main.js var team2Name) 
 
+var team1foul = 1; //= recieve val from sockt io (from admin/in-game-admin-panel/main.js var team1foul) 
+var team2foul = 2; //= recieve val from sockt io (from admin/in-game-admin-panel/main.js var team2foul)
+
+var team1set = 1; //= recieve val from sockt io (from admin/in-game-admin-panel/main.js var team1set) 
+var team2set = 0; //= recieve val from sockt io (from admin/in-game-admin-panel/main.js var team2set)
+
+
 $(document).ready(function () {
+
+    if (sporttype === 'Basketball') {
+        $('#team_a_customval').html("foul: " + team1foul);
+        $('#team_b_customval').html("foul: " + team2foul);
+        $('.score_customval_placeholder').show();
+        
+    }
+    if (sporttype === 'Volleyball') {
+        $('#team_a_customval').html("set: " + team1set);
+        $('#team_b_customval').html("set: " + team2set);
+        $('.score_customval_placeholder').show();
+        
+    }
+    
+
     if (team1Name === 'red') {
         $("#team-1-logo").attr("src", "../assets/vampire_logo.png");
         $("#team_a_title").html("Vampire");
