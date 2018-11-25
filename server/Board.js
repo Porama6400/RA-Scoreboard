65820de90f922152b3e43f9701e4f2b2d53e8a23
Porama Palittapongarnpim <PoramaTV@gmail.com>
2018-11-25 15:22:25 +0700

    More code clean-up

---
+++ server/Board.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Board = /** @class */ (function () {
+    function Board() {
+    }
+    return Board;
+}());
+exports.Board = Board;
+//# sourceMappingURL=Board.js.map
--- server/Server.js
+++ server/Server.js
@@ -5,35 +5,40 @@ var ClientHandler_1 = require("./ClientHandler");
 var Server = /** @class */ (function () {
     function Server(port) {
         var _this = this;
-        this.list = [];
+        this.clientHandlers = [];
+        this.boards = [];
+        this.purgeConnections = function () {
+            var purge = [];
+            var thisserver = _this;
+            _this.clientHandlers.forEach(function (client) {
+                if (!client.isAlive()) {
+                    purge.push(client);
+                    console.log("Client " + client.getIP() + " disconnected!");
+                    return;
+                }
+            });
+            purge.forEach(function (client) {
+                var index = thisserver.clientHandlers.indexOf(client);
+                thisserver.clientHandlers.splice(index, 1);
+            });
+        };
         this.tick = function () {
-            _this.purge();
+            _this.purgeConnections();
         };
         var thisserver = this;
         console.log("Server initializing...");
+        //Initialize Socket.io
         this.sockio = new SocketIO(port);
+        //Handle connection
         this.sockio.on('connection', function (sio) {
             var client = new ClientHandler_1.ClientHandler(sio);
             console.log("New connection from " + client.getIP());
-            thisserver.list.push(client);
+            thisserver.clientHandlers.push(client);
         });
+        //Initialize server ticking
+        setInterval(this.tick, 1000);
         console.log("Server listening on port " + port);
     }
-    Server.prototype.purge = function () {
-        var purge = [];
-        var thisserver = this;
-        this.list.forEach(function (client) {
-            if (!client.isAlive()) {
-                purge.push(client);
-                console.log("ClientHandler " + client.getIP() + " disconnected!");
-                return;
-            }
-        });
-        purge.forEach(function (client) {
-            var index = thisserver.list.indexOf(client);
-            thisserver.list.splice(index, 1);
-        });
-    };
     return Server;
 }());
 exports.Server = Server;
