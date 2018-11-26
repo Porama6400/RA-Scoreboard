6bfdfe208eb41ce0118052f336c2bcc477863754
Porama Palittapongarnpim <PoramaTV@gmail.com>
2018-11-26 11:09:49 +0700

    Update communication codes

---
+++ web/admin/in-game-admin-panel/admincommcore.js
@@ -0,0 +1,35 @@
+var server_addr = "ws://localhost:443";
+var socket = io(server_addr);
+var accessKey = undefined;
+var dataInHandler = function () {
+};
+
+//Logging conversation for debugging <3
+socket.on("sbr", (a) => console.log(a));
+socket.on("sbs", (a) => console.log(a));
+
+function use(key) {
+    socket.emit("sbr", {
+        req: 10,
+        payload: key
+    });
+}
+
+function sendRequest() {
+    socket.emit("sbs", {
+        req: 1
+    });
+}
+
+function pushScoreboard(data) {
+    socket.emit("sbs", {
+        req: 10,
+        payload: data
+    });
+}
+
+socket.on("sbs", function (din) {
+    if (din.req === 2) {
+        dataInHandler(din.payload);
+    }
+});
