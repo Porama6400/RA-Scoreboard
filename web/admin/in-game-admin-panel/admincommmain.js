773028605e1336cbf2128f4b5f2c36e333b38495
wit03 <40893632+wit03@users.noreply.github.com>
2018-11-26 12:37:44 +0700

    Sent data between page

--- web/admin/in-game-admin-panel/admincommmain.js
+++ web/admin/in-game-admin-panel/admincommmain.js
@@ -1,3 +1,15 @@
 dataInHandler =function (data) {
+    $('#team1-span').text(data.teamAname);
+    $('#team2-span').text(data.teamBname);
 
+    $('#team1-score').text(data.teamAscore);
+    $('#team2-score').text(data.teamBscore);
+
+    $('.team_customval').text(data.customValName);
+
+    $('#team-a-cval').text(data.teamAcval);
+    $('#team-b-cval').text(data.teamBcval);
+
+    sporttype = data.sporttype;
+    
 };
