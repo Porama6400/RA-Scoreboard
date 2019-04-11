f83d77f0c8664bd50950a3871aca4ec837935434
Jarukit Jintanasathirakul <witjarukit@gmail.com>
2019-04-11 18:53:34 +0700

    fix some error + add scss

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <img id="team-1-logo" className="logo" src="#"></img>
+        <img id="team-2-logo" className="logo" src="#"></img>
+        <div id="timer" className="timer">00.00</div>
+        <table>
+          <tbody>
+            <tr className="page_top_padding">
+              <td></td>
+              <td></td>
+              <td className="tablecolfixer"></td>
+              <td className="tablecolfixer"></td>
+              <td className="tablecolfixer"></td>
+              <td className="tablecolfixer"></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr className="score_placeholder">
+              <td></td>
+              <td colSpan={3} id="team_a_score">3</td>
+              <td colSpan={3} id="team_b_score">3</td>
+              <td></td>
+            </tr>
+            <tr className="score_customval_placeholder">
+              <td></td>
+              <td colSpan={3} className="team_customval"><span id="team-a-cval"></span></td>
+              <td colSpan={3} className="team_customval"><span id="team-b-cval"></span></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </table>
+        <table className="footer_bar">
+          <tbody>
+            <tr>
+              <td></td>
+              <td id="footer_connection_status"></td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div id="configpage">
+          Your access key: <span id="myaccesskey" >[Connecting...]</span><br />
+          <label htmlFor="mirrorkey">Mirroring key: </label><input type="text" id="mirrorkey" />
+          <button onClick="connectMirror($('#mirrorkey').val())">OK</button><br />
+          <button onClick="$('#configpage').hide()">Close</button>
+        </div>
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
