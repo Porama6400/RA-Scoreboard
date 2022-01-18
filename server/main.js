7e94b3a961f90a9bdcc79df9ee3a9e9ae3bad783
Porama6400 <poramatv@gmail.com>
2022-01-18 21:15:16 +0700

    project cleanup

--- server/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Server_1 = require("./Server");
-// let server: Server = new Server(6440, null, null);
-/**/
-var server = new Server_1.Server(6440, "/etc/letsencrypt/live/www.otlg.net/privkey.pem", "/etc/letsencrypt/live/www.otlg.net/fullchain.pem");
-/**/
-//# sourceMappingURL=main.js.map
